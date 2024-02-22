e02b9da10b267f62119bef193b08e7963b9c8904
hussain <hussainpazir@gmail.com>
2024-02-21 23:09:10 -0800

    java

---
+++ App.js
@@ -0,0 +1,113 @@
+// Chapter 1 start
+// Q1
+alert("Error please enter a valid password")
+// Q2
+alert("Welcome to JS Land \n Happy Coding")
+// Q3
+alert("Welcome to JS Land")
+// Q4
+alert("Hello....I can run JS through my webpage browser's console")
+// Chapter 1 ends
+
+
+// Chapter 2 start
+// Q1
+    var username="Hussain"
+    alert(username)
+
+// Q2
+    var firstname="Hussain"
+    var lastname=" Baloch"
+    alert(firstname + lastname)
+
+// Q3
+    var titledmessage="Hello World"
+    alert(titledmessage)
+
+// Q4
+    var name="John Doe"
+    alert(name)
+    var age="15 years old"
+    alert(age)
+    var institute="Certified Mobile Application Development"
+    alert(institute)
+
+// Q5
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P")
+// Q6
+    var text="My email adress is"
+    var email=" hussainbaloch@gmail.com"
+    alert(text + email)
+// Q7
+    var firsttext="I am trying to learn from the book"
+    var book=" A smarter way to learn JavaScript"
+    alert(firsttext + book)
+// Q8
+document.write("Yah ! I can write HTML content through JavaScript")
+
+// Q9
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// Chapter 2 end
+
+
+// Chapter 3 start
+
+// Q1
+    var text="I am"
+    var age=  15
+    var lasttext=" years old"
+    alert(text +" "+ age + lasttext) 
+
+// Q2
+var text="You have visited this site"
+var age=14
+var lasttext=" times"
+alert(text +" "+ age + lasttext) 
+// Q3
+    // var birthyear=1990
+    // document.write("My birth year is" ) 
+    // document.write(birthyear)
+// Q4
+ 
+// Chapter 3 end
+var  firstname="Hussain"
+alert(firstname)
+var  lastnamename="Hussain"
+alert(lastname) = "Hello, world!"
+
+var  Email="hussainbaloch@gmail.com"
+alert(Email)
+var phone="0321-3363979"
+alert ("You are learning JavaScript")
+alert ("Jawan Pakistan")
+// chapter 2
+// Q1
+var a="hussain baloch"
+// Q2a
+myVariableName="Hussain"
+// Q2b
+my_variable = None
+my_variable = "Jawan Pakistan"
+// Q2c
+var teamName;
+teamName = "Awesome Team"
+alert("Our team name is: " + teamName);
+var bestMan = "Charlie"
+bestMan = "John"
+// Chapter 3
+// Q3a
+var caseQty;
+// Q3b
+caseQty = 144;
+// Q3c
+var num = parseInt("9")
+// Q3d
+var sum = 5 + 7
+// Q3e
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge
+// Q3f
+var num = 10
+num += 5
